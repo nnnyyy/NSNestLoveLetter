befd8f86fea80094e743cfa40d4b0a6e64d2f979
genius180-PC\genius180 <genius180@naver.com>
2016-08-20 00:01:44 +0900

    센더, 리시버에 delegate 추가

--- Client/Assets/Scripts/Networks/Receiver.cs
+++ Client/Assets/Scripts/Networks/Receiver.cs
@@ -42,10 +42,10 @@ namespace NSNetwork
             }
         }
 
-        public delegate void LoginDelegate(GCPLoginRet.eResult result);
-        public static LoginDelegate onLoginCallback;
+        public delegate void LoginRetDelegate(GCPLoginRet.eResult result);
+        public static LoginRetDelegate onLoginRetCallback;
 
-        public static void OnLogin(GCPLoginRet packet)
+        static void OnLogin(GCPLoginRet packet)
         {
             switch (packet.result)
             {
@@ -55,42 +55,69 @@ namespace NSNetwork
                 case GCPLoginRet.eResult.NowConneting:         Debug.Log("Now Conneting..");                   break;
             }
 
-            if (onLoginCallback != null)
-                onLoginCallback(packet.result);
+            if (onLoginRetCallback != null)
+                onLoginRetCallback(packet.result);
         }
 
-        public static void OnRoomListRet(GCPRoomListRet packet)
+        public delegate void RoomListRetDelegate(GCPRoomListRet roomListRet);
+        public static RoomListRetDelegate onRoomListRetCallback;
+
+        static void OnRoomListRet(GCPRoomListRet packet)
         {
             Debug.Log("### OnRoomListRet ###");
             for( int i = 0; i < packet.listRooms.Count; ++i )
             {
                 Debug.Log("Room SN >> " + packet.listRooms[i].sn + ", Count >> " + (int)(packet.listRooms[i].userCount));
             }
+
+            if (onRoomListRetCallback != null)
+                onRoomListRetCallback(packet);
         }
 
-        public static void OnCreateRoomRet(GCPCreateRoomRet packet)
+        public delegate void CreateRoomRetDelegate(GCPCreateRoomRet.eReuslt result);
+        public static CreateRoomRetDelegate onCreateRoomRetCallback;
+
+        static void OnCreateRoomRet(GCPCreateRoomRet packet)
         {
             Debug.Log("### OnCreateRoomRet ###");
             if (packet.result == GCPCreateRoomRet.eReuslt.Success)
                 Debug.Log("Create Success >> " + packet.sn);
             else
                 Debug.Log("Create Fail..");
+
+            if (onCreateRoomRetCallback != null)
+                onCreateRoomRetCallback(packet.result);
         }
 
-        public static void OnEnterRoomRet(GCPEnterRoomRet packet)
+        public delegate void EnterRoomRetDelegate(GCPEnterRoomRet enterRoomRet);
+        public static EnterRoomRetDelegate onEnterRoomRetCallback;
+
+        static void OnEnterRoomRet(GCPEnterRoomRet packet)
         {
             Debug.Log("### OnEnterRoomRet ###");
             Debug.Log(packet.result);
             Debug.Log(packet.sn);
+
+            if (onEnterRoomRetCallback != null)
+                onEnterRoomRetCallback(packet);
         }
 
-        public static void OnLeaveRoomRet(GCPLeaveRoomRet packet)
+        public delegate void LeaveRoomRetDelegate(GCPLeaveRoomRet leaveRoomRet);
+        public static LeaveRoomRetDelegate onLeaveRoomRetCallback;
+
+        static void OnLeaveRoomRet(GCPLeaveRoomRet packet)
         {
             Debug.Log("### LeaveRoomRet ###");
             Debug.Log(packet.sn);
+
+            if (onLeaveRoomRetCallback != null)
+                onLeaveRoomRetCallback(packet);
         }
 
-        public static void OnRoomState(GCPRoomState packet)
+        public delegate void RoomStateDelegate(GCPRoomState roomState);
+        public static RoomStateDelegate onRoomStateCallback;
+
+        static void OnRoomState(GCPRoomState packet)
         {
             Debug.Log("### OnRoomState ###");
             Debug.Log(packet.flag);
@@ -98,23 +125,41 @@ namespace NSNetwork
             {
                 Debug.Log( "sn >> " + packet.listUsers[i].sn + ", ready >> " + (int)(packet.listUsers[i].readyState) );
             }
+
+            if (onRoomStateCallback != null)
+                onRoomStateCallback(packet);
         }
 
 
         // Game Love Letter
-        public static void OnLLStatus(GCPLLStatus packet)
+        public delegate void LLStatusDelegate(GCPLLStatus llStatus);
+        public static LLStatusDelegate onLLStatusCallback;
+
+        static void OnLLStatus(GCPLLStatus packet)
         {
             Debug.Log("### OnLLStatus ###");
+            if (onLLStatusCallback != null)
+                onLLStatusCallback(packet);
         }
 
-        public static void OnLLActionRet(GCPLLActionRet packet)
+        public delegate void LLActionRetDelegate(GCPLLActionRet llActionRet);
+        public static LLActionRetDelegate onLLActionRetCallback;
+
+        static void OnLLActionRet(GCPLLActionRet packet)
         {
             Debug.Log("### OnLLActionRet ###");
+            if (onLLActionRetCallback != null)
+                onLLActionRetCallback(packet);
         }
 
-        public static void OnLLRoundResult(GCPLLRoundResult packet)
+        public delegate void LLRoundResultDelegate(GCPLLRoundResult llRoundResult);
+        public static LLRoundResultDelegate onLLRoundResultCallback;
+
+        static void OnLLRoundResult(GCPLLRoundResult packet)
         {
             Debug.Log("### OnLLRoundResult ###");
+            if (onLLRoundResultCallback != null)
+                onLLRoundResultCallback(packet);
         }
     }
 }
