b49886d9ef492195963fb4ef3379d9878a71d97e
hogenius <hogeniuss@gmail.com>
2016-08-24 00:57:36 +0900

    인게임 구성중..

---
+++ Client/Assets/Scripts/Game/Card.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using NSNest.Common;
+using NSNetwork;
+
+namespace NSNest.Game
+{
+    //카드 선택시 이벤트
+    public delegate void CardPress(Card card);
+
+    //카드 정보
+    public class Card : MonoBehaviour
+    {
+        [SerializeField]
+        UISprite m_SprCard = null;
+
+        CardPress m_OnCardPress = null;
+        public event CardPress OnCardPress
+        {
+            add { m_OnCardPress += value; }
+            remove { m_OnCardPress -= value; }
+        }
+
+        public CardType CardType;
+
+        //카드의 보여짐 상태.
+        CardStatus m_CardStatus = CardStatus.Close;
+        public CardStatus CardStatus
+        {
+            get { return m_CardStatus; }
+            set
+            {
+                m_CardStatus = value;
+                SetStatus(m_CardStatus);
+            }
+        }
+
+        //카드의 위치
+        CardPosition m_CardPosition = CardPosition.Hand;
+        public CardPosition CardPosition
+        {
+            get { return m_CardPosition; }
+            set
+            {
+                m_CardPosition = value;
+                SetPosition(m_CardPosition);
+            }
+        }
+
+        public void SetCard(CardType cardType)
+        {
+            CardType = cardType;
+            CardStatus = CardStatus.Close;
+        }
+
+        void SetStatus(CardStatus CardStatus)
+        {
+            if (m_SprCard != null)
+                return;
+
+            if(CardStatus == CardStatus.Close)
+                m_SprCard.spriteName = CardInfo.GetCardImageName(CardType.Back);
+            else
+                m_SprCard.spriteName = CardInfo.GetCardImageName(CardType);
+        }
+
+        void SetPosition(CardPosition cardPosition)
+        {
+            //TODO 카드 포지션에 따라 카드 이동처리
+        }
+
+        public void OnPressCard()
+        {
+            if (m_OnCardPress != null)
+                m_OnCardPress(this);
+        }
+    }
+}
---
+++ Client/Assets/Scripts/Game/UserSlot.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using NSNest.Common;
+using NSNetwork;
+
+namespace NSNest.Game
+{
+    //유저와 1:1로 매칭되는 카드 처리스크립트
+    public class UserSlot : MonoBehaviour
+    {
+        //유저 고유 번호
+        [Range(1,4)]
+        public int UserNumber = 1;
+
+        List<Card> m_ListCard = new List<Card>();
+
+        public bool IsMyTurn = false;
+        
+        public void AddCard(CardType cardType)
+        {
+            //TODO : 카드 리소스 로딩하여 순서대로 추가
+        }
+
+        void Start()
+        {
+            BoardAgent.Instance.AddUserSlot(this);
+        }
+
+        void Update()
+        {
+            if(IsMyTurn)
+            {
+                //TODO : 내 차례 표시!!
+                Debug.Log("UserNumber " + UserNumber + " is Turn!!");
+            }
+            else
+            {
+                //TODO : 내 차례 아님 표시..
+            }
+        }
+    }
+}
