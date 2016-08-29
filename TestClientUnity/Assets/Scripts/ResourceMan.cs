fa1e1f677ed665773b1ff3a207bfa5d4e2d532a6
nnnyyy <nnnyyy@NNNYYY-PC>
2016-08-29 16:31:39 +0900

    인트로 페이지 구성

---
+++ TestClientUnity/Assets/Scripts/ResourceMan.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourceMan : Singleton<ResourceMan> {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Resource Manager Start");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ TestClientUnity/Assets/Scripts/common/Globals.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Globals : Singleton<Globals> {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Globals Start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
