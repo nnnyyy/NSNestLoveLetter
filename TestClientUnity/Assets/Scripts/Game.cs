62270876a07008c8d8e5ebcd8a71f6936c0debd0
DESKTOP-OIO0DRA\nnnyy <nnnyyy@nate.com>
2016-08-31 08:08:06 +0900

    테스트

--- TestClientUnity/Assets/Scripts/Game.cs
+++ TestClientUnity/Assets/Scripts/Game.cs
@@ -3,7 +3,19 @@ using System.Collections;
 using DG.Tweening;
 
 public class Game : MonoBehaviour {
+    static public Transform s_tfBaseForConv;
+    static public Transform s_tfRoot;
+    static public Vector3 ConvPosToRootLocal(Vector3 vSrcPos)
+    {
+        s_tfBaseForConv.position = vSrcPos;
+        return s_tfBaseForConv.localPosition;
+    }
 
+    static public Vector3 ConvPosToRootGlobal(Vector3 vSrcPos)
+    {
+        s_tfBaseForConv.position = vSrcPos;
+        return s_tfBaseForConv.position;
+    }
     public UserInfoBase[] m_InfoList;
     public Transform tfTestBase;
     public Transform tfBase;
@@ -12,26 +24,31 @@ public class Game : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        s_tfBaseForConv = tfTestBase;
+        s_tfRoot = tfBase;
         CardManager.Init();
         Card c = CardManager.CreateCard(Card.SizeType.LOCAL_HAND, 1);
         m_InfoList[0].PutHand(c);
-        c.transform.parent = tfBase.transform;
-        //c.transform.SetSiblingIndex(0);
-        tfTestBase.position = tfTestRemoteHand.position;
+        Card c2 = CardManager.CreateCard(Card.SizeType.LOCAL_HAND, 1);
+        m_InfoList[0].PutHand(c2);
+        Card c3 = CardManager.CreateCard(Card.SizeType.LOCAL_HAND, 1);
+        m_InfoList[0].PutHand(c3);
+        /*
+        c.transform.SetParent(s_tfRoot);
+        Vector3 vRoot = ConvPosToRootGlobal(tfTestRemoteHand.position);
         Sequence doSequence = DOTween.Sequence();
-        doSequence.Append(c.transform.DOLocalMove(tfTestBase.localPosition, 0.5f));
+        doSequence.Append(c.transform.DOMove(vRoot, 0.5f));
         doSequence.Join(c.transform.DOScale(0.25f, 0.25f));
-        tfTestBase.position = tfTestLocalHand.position;
-        doSequence.Append(c.transform.DOLocalMove(tfTestBase.localPosition, 0.5f));
+        vRoot = ConvPosToRootGlobal(tfTestLocalHand.position);
+        doSequence.Append(c.transform.DOMove(vRoot, 0.5f));
         doSequence.Join(c.transform.DOScale(0.5f, 0.5f)).OnComplete(()=> {
-            c.transform.parent = tfTestLocalHand;
+            
         });
-        
-
+        */
     }
 
     // Update is called once per frame
     void Update () {
-	
+        	
 	}
 }
--- TestClientUnity/Assets/Scripts/UserLocalInfo.cs
+++ TestClientUnity/Assets/Scripts/UserLocalInfo.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
 
 public class UserLocalInfo : UserInfoBase {
+
+    private List<Card> liCard = new List<Card>();
 
 	// Use this for initialization
 	void Start () {
@@ -16,8 +20,21 @@ public class UserLocalInfo : UserInfoBase {
     public override void PutHand(Card c)
     {
         base.PutHand(c);
-        c.transform.parent = m_panelHands.transform;
+        c.transform.SetParent(m_panelHands.transform);        
+        c.transform.localScale = new Vector3(0.5f, 0.5f, 1);
         c.transform.localPosition = new Vector3(0, 0, 1);
-        c.transform.localScale = new Vector3(0.5f, 0.5f, 1);        
+        liCard.Add(c);
+        SortCards();
+    }
+
+    void SortCards()
+    {
+        int idx = 0;
+        int nStart = 0 - ((int)m_panelHands.gameObject.GetComponent<RectTransform>().rect.width / 2 );
+        foreach(Card c in liCard)
+        {
+            c.transform.DOLocalMove(new Vector3(nStart + idx * 40, 0, 1), 0.5f);            
+            idx++;
+        }
     }
 }
